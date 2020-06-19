674d4a5cb3f43399f0b2efd4512a660c40b5fdbd
brooky56 <aidarahmetshin56@gmail.com>
2020-06-19 10:12:31 +0300

    new fixing

---
+++ Site/Estimation/CascadeResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site.Estimation
+{
+    public class CascadeResult
+    {
+        private static string _Name;
+        private static double _Prob;
+        public CascadeResult(string name, double prob)
+        {
+            _Name = name;
+            _Prob = prob;
+        }
+        public string Name { get; set; }
+
+        public double Prob { get; set; }
+    }
+}
